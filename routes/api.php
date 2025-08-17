93b6372051e2526833552cb08739be7a9814a771
ksandeleon <ksanureeves1@gmail.com>
2025-08-17 08:22:32 +0800

    add api endpoint docx

--- routes/api.php
+++ routes/api.php
@@ -6,16 +6,6 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\SurveyController;
 use App\Http\Controllers\Api\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 // Public routes (no authentication required)
 Route::prefix('v1')->group(function () {
